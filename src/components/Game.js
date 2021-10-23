91ce785ab988f3b638877aa5e5fc6cb7b5de1c31
Rukshan123 <rukzmadu@gmail.com>
2021-10-23 18:26:05 +0530

    Create game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import Board from './Board'
 
 export default function Game() {
-  return <></>
+  const status = 'Next Enter X'
+  const moves = (
+    <li>
+      <button>Start The Game</button>
+    </li>
+  )
+
+  const squares = Array(9).fill(null)
+
+  return (
+    <>
+      <div className='game-board'>
+        <Board squares={squares}> </Board>
+      </div>
+
+      <div className='game-info'>
+        <div>{status}</div>
+        <ul>{moves}</ul>
+      </div>
+    </>
+  )
 }
